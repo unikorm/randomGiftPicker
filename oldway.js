ebaf7accb682a6e04d56f2b00fa835bc44608d64
unikorm <adaled00@gmail.com>
2023-09-09 18:42:57 +0200

    snazim sa pridat validaciu ci je input empty ale mam tam nejaky problem with scoping of variables, i try it do without gpt so that is why it take so long

--- oldway.js
+++ oldway.js
@@ -3,13 +3,15 @@
 let check = console.log;
 
 let inputName = document.querySelector("#inputName");
+check(inputName);
 let selectedName = document.getElementById("selectedName");
 
 const buttonOnInput = document.getElementById("buttonOnInput");
 const listOfName = ["adam", "anna", "jana", "zuzka", "matus", "mama", "tato"];
 
 const getRandomName = () => {
-    inputName = inputName.value;
+    let inputNameValue = document.querySelector("#inputName").value;
+    check(inputNameValue);
     let nameBaseOnIndex;
     let randomNumber;
 
@@ -18,7 +20,7 @@ const getRandomName = () => {
         nameBaseOnIndex = listOfName[randomNumber];
     } while (nameBaseOnIndex === undefined || nameBaseOnIndex === inputName);
     // check(nameBaseOnIndex);
-    check(inputName.length);
+    check(inputNameValue.length);
 
     const reduceArray = () => {
         listOfName.splice(randomNumber, 1);
@@ -35,10 +37,12 @@ const showRandomName = () => {
 };
 
 buttonOnInput.addEventListener("click", () => {
-    if (inputName.value.length) {
-        return
-    };
-    showRandomName();
+    // if (inputName.value.length === 0) {
+    //     console.log("ajaj");
+    // } else {
+    //     showRandomName();
+    // };
     // check(listOfName);
+    showRandomName();
 });
 
