3975a9f72ad6afc58fda43a387001dc10a307430
unikorm <adaled00@gmail.com>
2023-09-10 23:27:05 +0200

    workin on randomizing of both arrays with our name, i have two versions, there're comments on that too, step by step with minimum of gpt

--- newway.js
+++ newway.js
@@ -8,9 +8,26 @@ const bttnRandom = document.getElementById("bttnCreateRandomConnect");
 const ulToShowIt = document.getElementById("resultOfRandominator");
 const listOfNamesOne = [...listOfNames];  // spread operator
 const listOfNamesTwo = Array.from(listOfNames);  // Array.from method
-// checkAgain(listOfNamesOne, listOfNamesTwo, bttnRandom, ulToShowIt);
+// checkAgain(listOfNamesOne, listOfNamesTwo);
+
+const randomizeArray = (array) => {    // randomize array's order (long version)
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temporaly = newArray[i];
+        newArray[i] = newArray[j];
+        newArray[j] = temporaly;
+    };
+    return newArray;
+};
+const randomArrayOne = randomizeArray(listOfNamesOne);   // results of this function
+const randomArrayTwo = randomizeArray(listOfNamesTwo);
+// checkAgain(randomArrayOne, randomArrayTwo);
+
 
 
 bttnRandom.addEventListener("click", () => {
-    checkAgain(listOfNamesOne, listOfNamesTwo);
+    const randomizeFirst = listOfNamesOne.sort(() => Math.random() - .5);   // randomize array's order (short version)
+    const randomizeSecond = listOfNamesTwo.sort(() => Math.random() - .5);  // and all in one line (function + result)
+    checkAgain(randomizeFirst, randomizeSecond);
 });
